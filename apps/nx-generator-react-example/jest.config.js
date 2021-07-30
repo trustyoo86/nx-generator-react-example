2f3d0f27154d7f30e63aae118f520914368fa3ae
Hanseung Yoo <hanseungyoo@Hanseungui-MacBookPro.local>
2021-07-31 00:55:14 +0900

    initial commit

---
+++ apps/nx-generator-react-example/jest.config.js
@@ -0,0 +1,10 @@
+module.exports = {
+  displayName: 'nx-generator-react-example',
+  preset: '../../jest.preset.js',
+  transform: {
+    '^(?!.*\\.(js|jsx|ts|tsx|css|json)$)': '@nrwl/react/plugins/jest',
+    '^.+\\.[tj]sx?$': 'babel-jest',
+  },
+  moduleFileExtensions: ['ts', 'tsx', 'js', 'jsx'],
+  coverageDirectory: '../../coverage/apps/nx-generator-react-example',
+};
